0e1654a570c1091e2c2e524cc6bc47d02d962a48
Alpha Diallo <alphaomar.diallo@gmail.com>
2022-06-24 09:19:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/realestatemanager/domain/Utils.kt
@@ -0,0 +1,45 @@
+package com.example.realestatemanager.domain
+
+import android.content.Context
+import android.net.wifi.WifiManager
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.roundToInt
+
+/**
+ * Created by Philippe on 21/02/2018.
+ */
+object Utils {
+    /**
+     * Conversion d'un prix d'un bien immobilier (Dollars vers Euros)
+     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
+     * @param dollars
+     * @return
+     */
+    fun convertDollarToEuro(dollars: Int): Int {
+        return (dollars * 0.812).roundToInt()
+    }
+
+    /**
+     * Conversion de la date d'aujourd'hui en un format plus approprié
+     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
+     * @return
+     */
+    val todayDate: String
+        get() {
+            val dateFormat: DateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+            return dateFormat.format(Date())
+        }
+
+    /**
+     * Vérification de la connexion réseau
+     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
+     * @param context
+     * @return
+     */
+    fun isInternetAvailable(context: Context): Boolean {
+        val wifi = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        return wifi.isWifiEnabled
+    }
+}
