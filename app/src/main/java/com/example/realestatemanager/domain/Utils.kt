220a28722d3bbc001125600907487bb2234819c7
Alpha Diallo <alphaomar.diallo@gmail.com>
2022-06-26 23:53:54 +0200

    NetworkManager implemented

--- app/src/main/java/com/example/realestatemanager/domain/Utils.kt
+++ app/src/main/java/com/example/realestatemanager/domain/Utils.kt
@@ -1,7 +1,13 @@
 package com.example.realestatemanager.domain
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import android.net.wifi.WifiManager
+import android.util.Log
+import androidx.core.content.ContextCompat.getSystemService
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
@@ -20,6 +26,9 @@ object Utils {
 
     private val dollarToEuroRate = Constant.DOLLARS_TO_EURO
     private val euroToDollarRate = Constant.EURO_TO_DOLLARS
+    private lateinit var networkRequest: NetworkRequest
+    private lateinit var networkCallback: ConnectivityManager.NetworkCallback
+    private lateinit var connectivityManager: ConnectivityManager
 
     fun convertDollarToEuro(dollars: Int): Int {
         return (dollars * dollarToEuroRate).roundToInt()
@@ -49,5 +58,41 @@ object Utils {
     fun isInternetAvailable(context: Context): Boolean {
         val wifi = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
         return wifi.isWifiEnabled
+    }
+
+    fun isInternetAvailable2 (context: Context) {
+        networkRequestCreation()
+        networkCallBackCreation()
+        connectivityManager = getSystemService(context, ConnectivityManager::class.java) as ConnectivityManager
+        Log.e("Utils", "isInternetAvailable2: ", )
+        connectivityManager.requestNetwork(networkRequest, networkCallback)
+        Log.e("Utils", "isInternetAvailable2: " + connectivityManager.requestNetwork(networkRequest, networkCallback))
+    }
+
+    private fun networkRequestCreation() {
+        networkRequest = NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+            .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+            .build()
+    }
+
+    private fun networkCallBackCreation () {
+        networkCallback = object :  ConnectivityManager.NetworkCallback() {
+             override fun onAvailable(network: Network) {
+                 super.onAvailable(network)
+             }
+
+             override fun onCapabilitiesChanged(
+                 network: Network,
+                 networkCapabilities: NetworkCapabilities
+             ) {
+                 super.onCapabilitiesChanged(network, networkCapabilities)
+             }
+
+             override fun onLost(network: Network) {
+                 super.onLost(network)
+             }
+        }
     }
 }
--- app/src/main/java/com/example/realestatemanager/ui/MainActivity.kt
+++ app/src/main/java/com/example/realestatemanager/ui/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.realestatemanager.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.realestatemanager.R
+import com.example.realestatemanager.domain.Utils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Utils.isInternetAvailable2(this)
     }
 }
