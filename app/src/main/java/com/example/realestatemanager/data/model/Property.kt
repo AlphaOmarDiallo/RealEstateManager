237d04120929e7827d6176db926d60001be81aa1
Alpha Diallo <alphaomar.diallo@gmail.com>
2022-07-04 13:18:18 +0200

    photos from Int to Byte

--- app/src/main/java/com/example/realestatemanager/data/model/Property.kt
+++ app/src/main/java/com/example/realestatemanager/data/model/Property.kt
@@ -1,5 +1,7 @@
 package com.example.realestatemanager.data.model
 
+import android.graphics.Bitmap
+
 data class Property(
     val id: Long,
     val type: String,
@@ -7,11 +9,11 @@ data class Property(
     val surface: Int,
     val numberOfRooms: Int,
     val description: String,
-    val photos: List<Int>,
+    val photos: List<Bitmap>,
     val address: String,
     val interestsAround: List<String>,
     val saleStatus: String,
     val onTheMarketSince: Long,
-    val offTheMarketSince:Long?,
-    val agentManagingProperty: Agent
+    val offTheMarketSince: Long?,
+    val agentManagingPropertyId: Int
 )
