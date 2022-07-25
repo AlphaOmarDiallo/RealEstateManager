be328a6064c8d38705b9bee6dcd0e2f9d1a44100
Alpha Diallo <alphaomar.diallo@gmail.com>
2022-07-25 14:29:59 +0200

    MyAccountActivity.kt layout done - settings to add

--- app/src/main/java/com/example/realestatemanager/ui/MyAccountActivity.kt
+++ app/src/main/java/com/example/realestatemanager/ui/MyAccountActivity.kt
@@ -3,10 +3,46 @@ package com.example.realestatemanager.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.realestatemanager.R
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
 
 class MyAccountActivity : AppCompatActivity() {
+
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
+    private val providers = arrayListOf(
+        AuthUI.IdpConfig.GoogleBuilder().build())
+
+    private val signInIntent = AuthUI.getInstance()
+        .createSignInIntentBuilder()
+        .setAvailableProviders(providers)
+        .build()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_account)
+
+        signInLauncher.launch(signInIntent)
     }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
+    }
+
 }
