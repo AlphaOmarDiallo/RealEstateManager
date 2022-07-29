125c8a489f3da0758b694ba8749da65df8960134
Alpha Diallo <alphaomar.diallo@gmail.com>
2022-07-29 14:30:30 +0200

    converters removed

--- app/src/main/java/com/example/realestatemanager/data/localData/Converters.kt
+++ app/src/main/java/com/example/realestatemanager/data/localData/Converters.kt
@@ -1,24 +1,10 @@
 package com.example.realestatemanager.data.localData
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import androidx.room.TypeConverter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import java.io.ByteArrayOutputStream
 
 class Converters {
-    @TypeConverter
-    fun fromBitmap(bitmap: Bitmap): ByteArray? {
-        val outputStream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
-        return outputStream.toByteArray()
-    }
-
-    @TypeConverter
-    fun toBitmap(byteArray: ByteArray): Bitmap? {
-        return BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
-    }
 
     @TypeConverter
     fun fromString(value: String?): List<String>? {
