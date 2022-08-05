8c8c61d98af6d287ebdf4f7fa486863516968f35
Alpha Diallo <alphaomar.diallo@gmail.com>
2022-08-05 08:15:39 +0200

    PhotoAdapter.kt from java to kotlin - loading photo correctly

--- app/src/main/java/com/example/realestatemanager/ui/adapter/PhotoAdapter.kt
+++ app/src/main/java/com/example/realestatemanager/ui/adapter/PhotoAdapter.kt
@@ -1,67 +1,60 @@
-package com.example.realestatemanager.ui.adapter;
+package com.example.realestatemanager.ui.adapter
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.realestatemanager.R
+import com.example.realestatemanager.data.model.InternalStoragePhoto
+import com.example.realestatemanager.databinding.PhotoItemBinding
+import com.example.realestatemanager.ui.adapter.PhotoAdapter.PhotoViewHolder
+import com.google.android.material.textfield.TextInputEditText
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.DiffUtil;
-import androidx.recyclerview.widget.ListAdapter;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.example.realestatemanager.R;
-import com.example.realestatemanager.data.model.InternalStoragePhoto;
-import com.example.realestatemanager.databinding.PhotoItemBinding;
-import com.google.android.material.textfield.TextInputEditText;
-
-public class PhotoAdapter extends ListAdapter<InternalStoragePhoto, PhotoAdapter.PhotoViewHolder> {
-
-    public PhotoAdapter(@NonNull DiffUtil.ItemCallback<InternalStoragePhoto> diffCallback) {
-        super(diffCallback);
+class PhotoAdapter(diffCallback: DiffUtil.ItemCallback<InternalStoragePhoto>) :
+    ListAdapter<InternalStoragePhoto, PhotoViewHolder>(diffCallback) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
+        val binding = PhotoItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PhotoViewHolder(binding.root)
     }
 
-    @NonNull
-    @Override
-    public PhotoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        PhotoItemBinding binding = PhotoItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
-        return new PhotoViewHolder(binding.getRoot());
+    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
+        holder.bind(getItem(position))
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull PhotoViewHolder holder, int position) {
-        holder.bind(getItem(position));
-    }
-
-    public static class ListDiff extends DiffUtil.ItemCallback<InternalStoragePhoto> {
-
-        @Override
-        public boolean areItemsTheSame(@NonNull InternalStoragePhoto oldItem, @NonNull InternalStoragePhoto newItem) {
-            return oldItem.getName().equals(newItem.getName());
+    class ListDiff : DiffUtil.ItemCallback<InternalStoragePhoto>() {
+        override fun areItemsTheSame(
+            oldItem: InternalStoragePhoto,
+            newItem: InternalStoragePhoto
+        ): Boolean {
+            return oldItem.name == newItem.name
         }
 
-        @Override
-        public boolean areContentsTheSame(@NonNull InternalStoragePhoto oldItem, @NonNull InternalStoragePhoto newItem) {
-            return oldItem.getName().equals(newItem.getName());
+        override fun areContentsTheSame(
+            oldItem: InternalStoragePhoto,
+            newItem: InternalStoragePhoto
+        ): Boolean {
+            return oldItem.name == newItem.name
         }
     }
 
-    public static class PhotoViewHolder extends RecyclerView.ViewHolder {
-
-        ImageView loadPhoto;
-        TextInputEditText description;
-        Button saveOrAdd;
-
-        public PhotoViewHolder(@NonNull View itemView) {
-            super(itemView);
-            loadPhoto = itemView.findViewById(R.id.ivLoadPhoto);
-            description = itemView.findViewById(R.id.TIETDescription);
-            saveOrAdd = itemView.findViewById(R.id.btnAddRemove);
+    class PhotoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var loadPhoto: ImageView
+        var description: TextInputEditText
+        var saveOrAdd: Button
+        fun bind(photo: InternalStoragePhoto?) {
+            description.setText(photo!!.name)
+            loadPhoto.load(photo.bmp)
         }
 
-        public void bind(InternalStoragePhoto photo) {
-            description.setText(photo.getName());
+        init {
+            loadPhoto = itemView.findViewById(R.id.ivLoadPhoto)
+            description = itemView.findViewById(R.id.TIETDescription)
+            saveOrAdd = itemView.findViewById(R.id.btnAddRemove)
         }
     }
 }
