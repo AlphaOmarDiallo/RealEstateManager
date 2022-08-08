9e19f6ebac28c9fd21294c9deb5432f602efc084
Alpha Diallo <alphaomar.diallo@gmail.com>
2022-08-08 04:52:48 +0200

    PhotoDao.kt and Photo.kt added

---
+++ app/src/main/java/com/example/realestatemanager/data/localData/PhotoDao.kt
@@ -0,0 +1,21 @@
+package com.example.realestatemanager.data.localData
+
+import androidx.room.*
+import com.example.realestatemanager.data.model.Photo
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PhotoDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPhoto(photo: Photo)
+
+    @Delete
+    suspend fun deletePhoto(photo: Photo)
+
+    @Query ("SELECT * FROM photo_table")
+    fun getListOfPhotos(): Flow<List<Photo>>
+
+    @Query("SELECT * FROM photo_table WHERE photo_id= :id")
+    fun getPhoto(id: Int): Flow<Photo>
+}
--- app/src/main/java/com/example/realestatemanager/data/model/Photo.kt
+++ app/src/main/java/com/example/realestatemanager/data/model/Photo.kt
@@ -1,5 +1,16 @@
 package com.example.realestatemanager.data.model
 
+import android.graphics.Bitmap
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "photo_table")
 data class Photo(
-    var name: String
-)
+    @ColumnInfo(name = "photo_bitmap")
+    var bmp: Bitmap
+) {
+    @PrimaryKey(autoGenerate = false)
+    @ColumnInfo(name = "photo_id")
+    val id: Int = 0
+}
